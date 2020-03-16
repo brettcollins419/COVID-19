d84fd02975726b96adabefe7dddb5b00498bef9d
brettcollins419 <brettcollins419@gmail.com>
2020-03-15 20:35:15 -0500

    visualize trends

--- covid19_eda.py
+++ covid19_eda.py
@@ -12,6 +12,8 @@ import time
 import re
 import string
 from itertools import chain
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 #%% FUNCTIONS
 ## ############################################################################
@@ -25,6 +27,62 @@ def readTimeSeriesData(path, case):
     return data
 
 
+#%% ENVIRONMENT
+## ############################################################################
+    
+stateAbrev = {
+    'AL' : 'Alabama',
+    'AK' : 'Alaska',
+    'AZ' : 'Arizona',
+    'AR' : 'Arkansas',
+    'CA' : 'California',
+    'CO' : 'Colorado',
+    'CT' : 'Connecticut',
+    'DE' : 'Delaware',
+    'FL' : 'Florida',
+    'GA' : 'Georgia',
+    'HI' : 'Hawaii',
+    'ID' : 'Idaho',
+    'IL' : 'Illinois',
+    'IN' : 'Indiana',
+    'IA' : 'Iowa',
+    'KS' : 'Kansas',
+    'KY' : 'Kentucky',
+    'LA' : 'Louisiana',
+    'ME' : 'Maine',
+    'MD' : 'Maryland',
+    'MA' : 'Massachusetts',
+    'MI' : 'Michigan',
+    'MN' : 'Minnesota',
+    'MS' : 'Mississippi',
+    'MO' : 'Missouri',
+    'MT' : 'Montana',
+    'NE' : 'Nebraska',
+    'NV' : 'Nevada',
+    'NH' : 'New Hampshire',
+    'NJ' : 'New Jersey',
+    'NM' : 'New Mexico',
+    'NY' : 'New York',
+    'NC' : 'North Carolina',
+    'ND' : 'North Dakota',
+    'OH' : 'Ohio',
+    'OK' : 'Oklahoma',
+    'OR' : 'Oregon',
+    'PA' : 'Pennsylvania',
+    'RI' : 'Rhode Island',
+    'SC' : 'south Carolina',
+    'SD' : 'South Dakota',
+    'TN' : 'Tennessee',
+    'TX' : 'Texas',
+    'UT' : 'Utah',
+    'VT' : 'Vermont',
+    'VA' : 'Virginia',
+    'WA' : 'Washington',
+    'WV' : 'West Virginia',
+    'WI' : 'Wisconsin',
+    'WY' : 'Wyoming',
+    }
+
 
 #%% TIME SERIES DATA INGESTION
 ## ############################################################################
@@ -37,7 +95,9 @@ timeSeriesReport = pd.concat([
     for case in ('Confirmed', 'Deaths', 'Recovered')
     ],
     axis = 0
-    )
+    ).fillna({'Province/State' : 'x'})
+
+
 
 
 timeSeriesReport['USstate'] = [
@@ -47,21 +107,107 @@ timeSeriesReport['USstate'] = [
     ]
 
 
+timeSeriesReport['Province/State_Agg'] = [
+    stateAbrev.get(st, loc)
+    for st, loc in 
+    timeSeriesReport[['USstate', 'Province/State']].values.tolist()
+    ]
+
 timeSeriesReportMelt = timeSeriesReport.melt(
-    id_vars = ['Province/State', 'Country/Region', 'Lat', 'Long', 'status', 'USstate'],
+    id_vars = ['Province/State', 'Country/Region', 
+               'Lat', 'Long', 
+               'status', 'USstate',
+               'Province/State_Agg'],
     var_name = 'date',
     value_name = 'cumlCount'
     )
     
 
+# Convert to timestamp
+timeSeriesReportMelt['date'] = pd.to_datetime(timeSeriesReportMelt['date'])
+
+
+# Align Confirmed, Recovered, and Death columns per day
 timeSeriesReportMeltPivot = timeSeriesReportMelt.pivot_table(
-    index = ['Province/State', 'Country/Region', 'Lat', 'Long', 'date', 'USstate'],
+    index = ['Province/State', 'Country/Region', 
+             'Lat', 'Long', 
+             'date', 'USstate',
+             'Province/State_Agg'],
     values = 'cumlCount',
     columns = 'status',
     aggfunc = np.sum,
-    fill_value = 0)
+    fill_value = 0
+    ).reset_index()
 
 
+
+
+# timeSeriesReportMeltPivot.to_csv(
+#     'output_data\\covid19_time_series_aligned.csv',
+#     index = False)
+
+# Group data by country
+timeSeriesCountry = (
+    timeSeriesReportMeltPivot
+        .groupby(['Country/Region', 'date'])
+        .agg({
+            'Lat' : np.mean,
+            'Long' : np.mean,
+            'Confirmed': np.sum,
+            'Deaths' : np.sum,
+            'Recovered' : np.sum
+             })
+        .reset_index()
+    )
+
+
+confirmedThreshold = 5
+
+# Dates where confirmed cases above threshold
+timeSeriesCountry['date_5cases'] = [
+    dte if confirmed >= confirmedThreshold
+    else None
+    for dte, confirmed in 
+    timeSeriesCountry[['date', 'Confirmed']].values.tolist()
+    ]
+
+# Date of first case and total # of cases for each country
+countryCases = (
+    timeSeriesCountry[timeSeriesCountry['Confirmed'] >= 1]
+        .groupby(['Country/Region'])
+        .agg({'date' : np.min,
+              'date_5cases' : np.min,
+              'Confirmed' : np.max
+              })
+        .to_dict('index')
+    )
+
+
+timeSeriesCountry['daysSinceFirstCase'] = [
+    max((dte - countryCases.get(country).get('date_5cases', 'date')).days, 0)
+    for country, dte in 
+    timeSeriesCountry[['Country/Region', 'date']].values.tolist()
+    ]
+        
+
+
+
+#%% VISUALIZE MOST IMPACTED COUNTRIES
+## ###########################################################################
+
+fig, ax = plt.subplots(1)
+
+sns.lineplot(x = 'daysSinceFirstCase',
+             y = 'Confirmed',
+             hue = 'Country/Region',
+             data = timeSeriesCountry[
+                 [(countryCases.get(country)['Confirmed'] > 1000)
+                  for country in timeSeriesCountry['Country/Region'].values.tolist()
+                  ]],
+             ax = ax)
+
+plt.grid()
+plt.tight_layout()
 
 #%% DAILY REPORT DATA INGESTION
 ## ############################################################################
@@ -76,3 +222,5 @@ dailyReport = pd.concat([
     sort = True
     )
 
+
+x = dailyReport['Last Update'].dtype
